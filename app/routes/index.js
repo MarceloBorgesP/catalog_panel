8aeda41c758b5cb8cd93f07677dc08e01500265a
Marcelo Borges <borgespereira.marcelo@gmail.com>
2016-10-19 10:37:03 -0200

    fix broken test

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,9 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   activate: function(){
-    Ember.$('body').toggleClass("home")
+    Ember.$('body').toggleClass("home");
   },
   deactivate: function(){
-    Ember.$('body').toggleClass("home")
+    Ember.$('body').toggleClass("home");
   }
 });
